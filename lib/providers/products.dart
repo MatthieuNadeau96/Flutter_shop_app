748574128454af2551ffe752919a9439d2aa0664
Matthieu <matthieunadeau96@gmail.com>
2020-04-09 11:37:06 -0400

    Handling Errors Gracefully

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -69,24 +69,32 @@ class Products with ChangeNotifier {
 
   Future<void> addProduct(Product product) {
     const url = 'https://flutter-shop-app-9a5c9.firebaseio.com/products.json';
-    return http.post(url, body: json.encode({
-      'title': product.title,
-      'description': product.description,
-      'imageUrl': product.imageUrl,
-      'price': product.price,
-      'isFavorite': product.isFavorite,
-    }),).then((response) {
+    return http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
       // print(json.decode(response.body));
       final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: json.decode(response.body)['name'],
-    );
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // at the start of the list
-    notifyListeners();
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the start of the list
+      notifyListeners();
+    }).catchError((error) {
+      print(error);
+      throw error;
     });
   }
 
