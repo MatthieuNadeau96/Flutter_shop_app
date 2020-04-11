b42c6d3c003ad5acdb95ded17d749e3fd55f24ce
Matthieu <matthieunadeau96@gmail.com>
2020-04-11 11:34:56 -0400

    Utilizing Optimistic Updating

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -139,7 +139,16 @@ class Products with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url = 'https://flutter-shop-app-9a5c9.firebaseio.com//products/$id';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    http.delete(url).then((_) {
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
+    _items.removeAt(existingProductIndex);
     notifyListeners();
   }
 }
